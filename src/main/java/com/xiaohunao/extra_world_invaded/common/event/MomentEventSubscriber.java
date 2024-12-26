14f574acb9a032e4c3b79bd3bc2d0078c6a3d977
westernat <westernat233@163.com>
2024-12-26 14:51:35 +0800

    制箭台JEI，改gelstone为spore_root

--- src/main/java/com/xiaohunao/extra_world_invaded/common/event/MomentEventSubscriber.java
+++ src/main/java/com/xiaohunao/extra_world_invaded/common/event/MomentEventSubscriber.java
@@ -1,8 +1,6 @@
 package com.xiaohunao.extra_world_invaded.common.event;
 
-import net.neoforged.fml.common.EventBusSubscriber;
-
-@EventBusSubscriber
-public class MomentEventSubscriber {
-
-}
+//@EventBusSubscriber
+//public class MomentEventSubscriber {
+//
+//}
